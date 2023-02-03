87ac4657fb86c9b3c5afb342ad1e657e7fb55b02
Jasii1 <jasiandrade1@gmail.com>
2023-02-02 20:25:03 -0600

    final js additions, got local storage working

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-const btn = document.getElementById("saveBtn");
 
-setInterval(function(today){
+$(document).ready(function () {
+
+  setInterval(function(today){
   var today = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a'); //current time and day
   $(currentDay).text(today);
 },1000);
@@ -13,11 +14,9 @@ setInterval(function(today){
 
 var allTimeBlocks = document.querySelectorAll(".time-block")
 
- var currentTime = 11             //dayjs().format('HH');
-console.log(currentTime)
+ var currentTime = dayjs().format('HH');
 
 for (var i = 0; i < allTimeBlocks.length; i++) {  // for loop
- console.log(allTimeBlocks[i].id)
  var timeBlockHour = parseInt(allTimeBlocks[i].id)
  if (currentTime > timeBlockHour)  {
   allTimeBlocks[i].classList.add("past")
@@ -27,63 +26,53 @@ for (var i = 0; i < allTimeBlocks.length; i++) {  // for loop
   allTimeBlocks[i].classList.add ("present")
  }
 }
+    
 
-let scheduler = [
-  {
-    time: "8am",
-    num: 8,
-    text: ""
-  },
-  {
-    time: "9am",
-    num: 9,
-    text: ""
-  },
-  {
-    time: "10am",
-    num: 10,
-    text: ""
-  },
-  {
-    time: "11am",
-    num: 11,
-    text: ""
-  },
-  {
-    time: "12pm",
-    num: 12,
-    text: ""
-  },
-  {
-    time: "1pm",
-    num: 13,
-    text: ""
-  },
-  {
-    time: "2pm",
-    num: 14,
-    text: ""
-  },
-  {
-    time: "3pm",
-    num: 15,
-    text: ""
-  },
-  {
-    time: "4pm",
-    num: 16,
-    text: ""
-  },
-  {
-    time: "5pm",
-    num: 17,
-    text: ""
-  }
-  ];  
+$(function (){
 
+    // function locallyStored() {
+    //   console.log(this)
+    //   var input = $(this).siblings(".description").val(); 
+    //   localStorage.setItem('string', input.val);
+    //   console.log(this)
+    // }
+
+  //   for (var i = 0 ; i < btn.length; i++) {
+  //     btn[i].addEventListener('click' , locallyStored) ; 
+  //     console.log(this)
+
+  //     input[i]
+
+  //  }
+//jquery
+    $(".saveBtn").on("click", function() {
+
+      var input = $(this).siblings(".description").val(); 
+      var timeBlock = $(this).parent().attr("id");
+      console.log(this)
+
+      localStorage.setItem(timeBlock, input);
+    })
+
+    $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+  })
+})
+
+
+ 
+
+ 
 //Code to change color depending on past or present using military time
-$(function () {
-  
+
 
 
 
@@ -102,5 +91,4 @@ $(function () {
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-});
+  // attribute of each time-block be used to do this
